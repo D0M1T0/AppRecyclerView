63a2dcb333e8b18254eadd10f0c3c8f92229d567
D0M1T0 <florireyes150@gmail.com>
2024-02-26 14:34:36 -0500

    Ejercicio finishiado

--- app/src/main/java/com/example/apprecyclerview/Adapter_Android.kt
+++ app/src/main/java/com/example/apprecyclerview/Adapter_Android.kt
@@ -1,4 +1,39 @@
 package com.example.apprecyclerview
 
-class Adapter_Android {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apprecyclerview.databinding.ActivityMainBinding
+import com.example.apprecyclerview.databinding.AndroidItemBinding
+import kotlin.coroutines.coroutineContext
+
+class Adapter_Android (val ListaAndroid:List<Android>, val context:Context)
+    : RecyclerView.Adapter<Adapter_Android.ViewHolder>() {
+    inner class ViewHolder(val binding: AndroidItemBinding)
+        :RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = AndroidItemBinding
+            .inflate(LayoutInflater.from(parent.context),parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount() = ListaAndroid.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder){
+            with(ListaAndroid[position]){
+                binding.tvnomversion.text = nombre
+                binding.tvnumversion.text = version
+                binding.ivandroid.setImageResource(img)
+                binding.cvandroid.setOnClickListener(View.OnClickListener {
+                    Toast.makeText(context, "Click Item Android $nombre",
+                        Toast.LENGTH_LONG).show()
+                })
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/apprecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/apprecyclerview/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.apprecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.apprecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvandroid.layoutManager = LinearLayoutManager(applicationContext)
+        binding.rvandroid.adapter = Adapter_Android(
+            ListaAndroid(), applicationContext)
+    }
+    fun ListaAndroid (): List<Android>{
+        var lista = ArrayList<Android>()
+        lista.add(Android("Cup Cake", "Android 1.0", R.drawable.image1))
+        lista.add(Android("Petit Four", "Android 1.1", R.drawable.image2))
+        lista.add(Android("Apple Pie", "Android 1.5", R.drawable.image3))
+        lista.add(Android("Donut", "Android 1.6", R.drawable.image4))
+        lista.add(Android("Eclair", "Android 2.0 - 2.1", R.drawable.image5))
+        lista.add(Android("Froyo", "Android 2.2 - 2.2.3", R.drawable.image6))
+        lista.add(Android("GingerBread", "2.3 - 2.3.7", R.drawable.image7))
+        lista.add(Android("HoneyComb", "Android 3.0 - 3.2.6", R.drawable.image8))
+        lista.add(Android("Jelly Bean", "Android 4.1 - 4.3.1", R.drawable.image9))
+        lista.add(Android("Marshmallow", "Android 6.0 - 6.0.1", R.drawable.image10))
+        return lista
     }
 }
